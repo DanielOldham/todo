bf4fa97c6526ccfcca7cd67fdd62139df574dc8d
Daniel Oldham <dro@ncbt.org>
2025-03-13 12:51:17 -0600

    added core app boilerplate, added basic signup and login functionality

---
+++ todo/core/forms.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class SignupForm(UserCreationForm):
+    """
+    Django UserCreationForm.
+    Sets up user creation form and adds CSS bootstrap classes and HTML placeholders.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Loops through fields and adds a bootstrap form-control class
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
+        self.fields['username'].widget.attrs.update({'placeholder': 'type your username'})
+        self.fields['email'].widget.attrs.update({'placeholder': 'type your email'})
+        self.fields['first_name'].widget.attrs.update({'placeholder': 'type your first name'}, required=True)
+        self.fields['last_name'].widget.attrs.update({'placeholder': 'type your last name'}, required=True)
+        self.fields['password1'].widget.attrs.update({'placeholder': 'type your password'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'confirm your password'})
+
+    class Meta:
+        """
+        Django Meta inner class.
+        """
+        model = User
+
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+
+class LoginForm(AuthenticationForm):
+    """
+    Django AuthenticationForm.
+    Sets up authentication form and adds CSS bootstrap classes and HTML placeholders.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Loops through fields and adds a bootstrap form-control class
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
+        self.fields['username'].widget.attrs.update({'placeholder': 'type your username'})
+        self.fields['password'].widget.attrs.update({'placeholder': 'type your password'})
+
+    class Meta:
+        model = User
+
+        fields = ['username', 'password']
---
+++ todo/core/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from .forms import SignupForm, LoginForm
+from django.contrib import auth
+
+
+def login(request):
+    """
+    Django view.
+    Display login page to user.
+
+    :param request: Django request
+    :return: rendered login template if request is GET (or if there are errors), else returns redirect to to_do list view
+    """
+
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+    if request.method == 'POST':
+        form = LoginForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('')
+        else:
+            # errors logging in
+            return render(request, 'login.html', {'form': form})
+
+def signup(request):
+    """
+    Django view.
+    Display signup page.
+
+    :param request: Django request
+    :return: rendered signup template if request is GET (or there are errors), else return redirect to dashboard view
+    """
+
+    if request.method == 'GET':
+        form = SignupForm()
+        return render(request, 'signup.html', {'form': form})
+
+    if request.method == 'POST':
+        form = SignupForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('')
+        else:
+            # user is not created
+            return render(request, 'signup.html', {'form': form})
