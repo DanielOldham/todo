0a2fa5e5c2da47e8337c742efbae440ca75358f6
Daniel Oldham <dro@ncbt.org>
2025-03-16 15:40:56 -0600

    removed other unnneeded docker compose files and unneeded requirements file

--- todo/entrypoint.prod.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "$POSTGRES_PASSWORD" | psql --host dbprod --user postgres -W template1 -c "create role $DATABASE_USER with login password '$DATABASE_PASSWORD'"
-echo "$POSTGRES_PASSWORD" | psql --host dbprod --user postgres -W template1 -c "create database $DATABASE_NAME with owner $DATABASE_USER"
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-python -m gunicorn --bind 0.0.0.0:8000 --workers 3 todo.wsgi:application
